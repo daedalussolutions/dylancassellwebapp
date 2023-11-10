e35866ec5618bc82101a2f370a95f67a9230ce8a
daedalussolutions <dylancassell000@hotmail.com>
2023-11-10 17:40:03 -0500

    Resonsive CSS (50% done)

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import '../App.css';
+import { motion } from "framer-motion";
+import { AnimatePresence } from 'framer-motion';
 
 const FrontPage = () => {
     return(
-    <div className="front-page-container">
-       <h1 title='DYLAN CASSELL'>DYLAN CASSELL</h1>
-        <h3>Fullstack Developer</h3>
-    </div>
+        <div className="front-page-container">
+            <motion.div
+            initial={{ opacity:0 }}
+            animate={{ opacity:1 }}
+            transition={ {duration: 0.5, ease: "easeIn"}}>
+                <h1 title='DYLAN CASSELL'>DYLAN CASSELL</h1>
+                <div className='mobile-name-display'>
+                    <h1 title='DYLAN'>DYLAN</h1>
+                    <h1 title='CASSELL'>CASSELL</h1>
+                </div>
+            </motion.div>
+            <AnimatePresence>
+                <motion.div
+                initial={{ x: -1500}}
+                animate={{ x: 0}}
+                transition={{delay: 3}}
+                >
+                    <div>
+                    <h3>Fullstack Developer</h3>
+                    </div>
+                </motion.div>
+            </AnimatePresence>
+        </div>
     );
 }
 
